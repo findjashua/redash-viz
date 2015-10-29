4568be6f2cd41f6e51c56b26e186f6afeb1b0406
Jashua Gupta <findjashua+sourcetree@gmail.com>
2015-10-28 18:06:38 -0700

    make synthEvt optional param in publish

--- src/lib/publish.js
+++ src/lib/publish.js
@@ -2,5 +2,6 @@ import subject from './subject'
 
 export default (payload, synthEvt) => {
   if (payload.preventDefault) synthEvt.preventDefault()
-  subject.onNext(Object.assign(payload, { synthEvt }))
+  if (synthEvt) Object.assign(payload, { synthEvt })
+  subject.onNext(payload)
 }
